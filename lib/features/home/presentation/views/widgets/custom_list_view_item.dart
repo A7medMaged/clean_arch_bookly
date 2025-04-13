a5e885cc4b54ec1089aca3d9346d46efa3c116bd
AhmedMaged1722 <am9351705@gmail.com>
2025-04-13 16:31:39 +0200

    - Add CustomListViewItem widget for displaying list items with image and styling

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,27 @@
+import 'package:bookly_ca/core/helper/assets.dart';
+import 'package:bookly_ca/core/helper/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(AssetsData.test),
+              fit: BoxFit.fill,
+            ),
+            color: kBlackColor,
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
+    );
+  }
+}
