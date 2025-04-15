51b56dbd89da748499f110736723d1457e17023d
AhmedMaged1722 <am9351705@gmail.com>
2025-04-15 22:38:41 +0200

    - Add BookEntity and HomeRepo classes for book management

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,17 @@
+class BookEntity {
+  final String title;
+  final String authorName;
+  final String description;
+  final String image;
+  final num price;
+  final num rating;
+
+  BookEntity(
+    this.image,
+    this.price,
+    this.rating,
+    this.title,
+    this.authorName,
+    this.description,
+  );
+}
