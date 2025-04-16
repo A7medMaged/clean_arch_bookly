f8cbc0815f56538dbb12587588e265648c452f0b
AhmedMaged1722 <am9351705@gmail.com>
2025-04-16 22:30:31 +0200

    - Refactor BookModel and BookEntity to enhance data structure and initialization

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,17 +1,17 @@
 class BookEntity {
+  final String bookId;
   final String title;
-  final String authorName;
-  final String description;
-  final String image;
-  final num price;
-  final num rating;
+  final String? authorName;
+  final String? image;
+  final num? price;
+  final int pageCount;
 
-  BookEntity(
-    this.image,
-    this.price,
-    this.rating,
-    this.title,
-    this.authorName,
-    this.description,
-  );
+  BookEntity({
+    required this.bookId,
+    required this.image,
+    required this.price,
+    required this.pageCount,
+    required this.title,
+    required this.authorName,
+  });
 }
