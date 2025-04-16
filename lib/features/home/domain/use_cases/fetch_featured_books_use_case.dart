48957f85928ef84e411f559561b965de98b6da1f
AhmedMaged1722 <am9351705@gmail.com>
2025-04-16 17:27:51 +0200

    - Refactor FetchFeaturedBooksUseCase to extend UseCase and implement call method

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,14 +1,17 @@
+import 'package:bookly_ca/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
 import 'package:bookly_ca/core/errors/failure.dart';
 import 'package:bookly_ca/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_ca/features/home/domain/repos/home_repo.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
   FetchFeaturedBooksUseCase(this.homeRepo);
+  
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async{
+    return await homeRepo.fetchFeaturedBooks();
 
-  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks(){
-    return homeRepo.fetchFeaturedBooks();
   }
 }
